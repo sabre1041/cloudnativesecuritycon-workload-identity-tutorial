314a3a0632ef50cf5539d126194b46e94cd184ad
Mariusz Sabath <mrsabath@gmail.com>
2024-06-25 20:49:57 -0700

    Add py-service-account to py app
    Signed-off-by: Mariusz Sabath <mrsabath@gmail.com>

--- resources/secure/vault/vault-oidc.sh
+++ resources/secure/vault/vault-oidc.sh
@@ -77,7 +77,7 @@ EOF
       "bound_audiences": "vault",
       "bound_claims_type": "glob",
       "bound_claims": {
-        "sub":"spiffe://${APP_DOMAIN}/ns/workload-identity-tutorial/sa/*"
+        "sub":"spiffe://${APP_DOMAIN}/ns/workload-identity-tutorial/sa/py-service-account"
       },
       "token_ttl": "24h",
       "token_policies": "$POLICY"
